b217aa90456aa7de094d6ab1ba60135d8f47756f
Arnon Axelrod <arnonaxelrod@hotmail.com>
2020-11-28 19:01:00 +0200

    Sans stop moving after ball hits him

--- ColorsBall/Form1.cs
+++ ColorsBall/Form1.cs
@@ -15,6 +15,7 @@ namespace ColorsBall
 	public partial class frmMain : Form
 	{
 		int dx=10, dy=10;
+		bool stop = false;
 
 		public frmMain()
 		{
@@ -24,34 +25,39 @@ namespace ColorsBall
 		private void tmrBall_Tick(object sender, EventArgs e)
 		{
 			// Move ball
-			pctBall.Left+=dx;
-			pctBall.Top+=dy;
+			pctBall.Left += dx;
+			pctBall.Top += dy;
 			if (pctBall.Bottom >= pnlArena.ClientSize.Height || pctBall.Top < 0)
 				dy = -dy;
 			if (pctBall.Right >= pnlArena.ClientSize.Width || pctBall.Left < 0)
 				dx = -dx;
 
 			// Move player
-			if (Keyboard.IsKeyDown(Key.Up))
-				pctPlayer.Top-=10;
-			if (Keyboard.IsKeyDown(Key.Down))
-				pctPlayer.Top += 10;
-			if (Keyboard.IsKeyDown(Key.Left))
-				pctPlayer.Left -= 10;
-			if (Keyboard.IsKeyDown(Key.Right))
-				pctPlayer.Left += 10;
-			if (pctPlayer.Top <= 0)
-				pctPlayer.Top = 0;
-			if (pctPlayer.Left <= 0)
-				pctPlayer.Left = 0;
-			if (pctPlayer.Bottom >= pnlArena.ClientSize.Height)
-				pctPlayer.Top = pnlArena.ClientSize.Height - pctPlayer.Height;
-			if (pctPlayer.Right >= pnlArena.ClientSize.Width)
-				pctPlayer.Left = pnlArena.ClientSize.Width - pctPlayer.Width;
-
+			if (!stop)
+			{
+				if (Keyboard.IsKeyDown(Key.Up))
+					pctPlayer.Top -= 10;
+				if (Keyboard.IsKeyDown(Key.Down))
+					pctPlayer.Top += 10;
+				if (Keyboard.IsKeyDown(Key.Left))
+					pctPlayer.Left -= 10;
+				if (Keyboard.IsKeyDown(Key.Right))
+					pctPlayer.Left += 10;
+				if (pctPlayer.Top <= 0)
+					pctPlayer.Top = 0;
+				if (pctPlayer.Left <= 0)
+					pctPlayer.Left = 0;
+				if (pctPlayer.Bottom >= pnlArena.ClientSize.Height)
+					pctPlayer.Top = pnlArena.ClientSize.Height - pctPlayer.Height;
+				if (pctPlayer.Right >= pnlArena.ClientSize.Width)
+					pctPlayer.Left = pnlArena.ClientSize.Width - pctPlayer.Width;
+			}
 			// Check if ball hits the player
 			if (pctPlayer.Bounds.IntersectsWith(pctBall.Bounds))
+			{
 				pctPlayer.Image = Properties.Resources.sans_hit;
+				stop = true;
+			}
 		}
 	}
 }
